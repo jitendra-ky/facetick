cd6991637749506c9316dabeb42ea17c9a0c5d5b
zsquare12 <36557466+zsquare12@users.noreply.github.com>
2024-05-21 12:58:59 +0530

    improve the code of components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,58 @@
 this hold the project section 
  */
 
+const projects = {
+    SpyBat: {
+        imgSrc: "static/images/bat.jpg",
+        imgAlt: "boy studying",
+        skill: "Python",
+        description: "It will suck all of your passwords without even letting you know.",
+        codeLink: "code/spy_bat.html"
+    },
+    RevisionPredictor: {
+        imgSrc: "static/images/revision.jpg",
+        imgAlt: "boy studying",
+        skill: "Python",
+        description: "Ohh! God what a masterpiece I create. Isn't this proof that I am smart? Ok! Ok! Ok! Back on topic. This application loses a big burden over my head about what I have to revise and when to keep my concept clear.",
+        codeLink: "code/revision_predictor.html"
+    },
+    SmilePleaseLife: {
+        imgSrc: "static/icons/smileplease_project.png",
+        imgAlt: "boy studying",
+        skill: "HTML CSS JS",
+        description: "Embark on a visual journey through my diverse projects, where each pixel tells a story of passion and proficiency. Among them, find the heart of it all - my portfolio website. It's not just a project; it's a reflection of my dedication to crafting digital experiences.",
+        codeLink: "You are on the same website"
+    },
+    SharkToDo: {
+        imgSrc: "static/icons/sharktodo.svg",
+        imgAlt: "boy studying",
+        skill: "Full Stack Project",
+        description: "Effortlessly manage tasks in your personal oasis. Join SmilePlease.Life for exclusive access to SharkToDo's secure, tailored task management.",
+        codeLink: "sharktodo.html"
+    }
+};
+
+const projects_jsx = (<>
+    {Object.entries(projects).map(([key, value], index) => (
+        <div className="card_container">
+            <img src={value.imgSrc} alt={value.imgAlt} className="card_img" />
+            <div className="card_body">
+                <div className="card_body_top">
+                    <h5>{ value.skill}</h5>
+                    <h1>SpyBat</h1>
+                </div>
+                <p>{value.description}</p>
+            </div>
+            <div className="card_footer">
+                <a href={value.codeLink}><i className="fa-solid fa-code"></i>code</a>
+            </div>
+        </div>
+    ))}
+</>)
+
+console.log(projects);
+
+
 function Projects() {
 
     return (<>
@@ -10,70 +62,8 @@ function Projects() {
             <h1>projects</h1>
             <p>that's how I spend my leisure time to carve out something that help me and improve my skill:) </p>
             <div className="projects">
-                <div className="card_container">
-                    <img src="static/images/bat.jpg" alt="boy studing" className="card_img"/>
-                        <div className="card_body">
-                            <div className="card_body_top">
-                                <h5>Python</h5>
-                                <h1>SpyBat</h1>
-                            </div>
-                            <p>It will suck all of your passwords without even letting you know.</p>
-                        </div>
-                        <div className="card_footer">
-                            <a href="code/spy_bat.html"><i className="fa-solid fa-code"></i>code</a>
-                        </div>
-                </div>
-                <div className="card_container">
-                    <img src="static/images/revision.jpg" alt="boy studing" className="card_img"/>
-                        <div className="card_body">
-                            <div className="card_body_top">
-                                <h5>Python</h5>
-                                <h1>RevisionPredictor</h1>
-                            </div>
-                            <p>Ohh! God what a masterpiece I create. Isn't this proof that I am smart? Ok! Ok! Ok! Back on
-                                topic. This application
-                                loses a big burden over my head about what I have to revise and when to keep my concept
-                                clear.</p>
-                        </div>
-                        <div className="card_footer">
-                            <a href="code/revision_predictor.html"><i className="fa-solid fa-code"></i>code</a>
-                        </div>
-                </div>
-                <div className="card_container">
-                    <img src="static/icons/smileplease_project.png" alt="boy studing" className="card_img"/>
-                        <div className="card_body">
-                            <div className="card_body_top">
-                                <h5>HTML CSS JS</h5>
-                                <h1>SmilePlease.Life</h1>
-                            </div>
-                            <p>Embark on a visual journey through my diverse projects, where each pixel tells a story of
-                                passion and proficiency. Among
-                                them, find the heart of it all - my portfolio website. It's not just a project; it's a
-                                reflection of my dedication to
-                                crafting digital experiences.</p>
-                        </div>
-                        <div className="card_footer">
-                            <h3>you are on same website</h3>
-                        </div>
-                </div>
-                <div className="card_container">
-                    <img src="static/icons/sharktodo.svg" alt="boy studing" className="card_img"/>
-                        <div className="card_body">
-                            <div className="card_body_top">
-                                <h5>Full Stack Project</h5>
-                                <h1>SharkToDo</h1>
-                            </div>
-                            <p>Effortlessly manage tasks in your personal oasis. Join SmilePlease.Life for exclusive access
-                                to SharkToDo's secure,
-                                tailored task management.</p>
-                        </div>
-                        <div className="card_footer">
-                            <a href="sharktodo.html">check it:)</a>
-                        </div>
-                </div>
-
+                {projects_jsx}
             </div>
-
         </section>
     </>)
 }
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -2,6 +2,25 @@
 this is from skill section 
 */
 
+const my_skills = {
+    "Hackerrank Python Basic":          "static/images/hackerrank_python_basic.png",
+    "Python Core Sololearn":            "static/images/python_core_sololearn.png",
+    "Python Intermediate Sololearn":    "static/images/python_intermediate_sololearn.png",
+    "Data Structures Python Sololearn": "static/images/DS_python_sololearn.png",
+    "Python for Beginners Sololearn":   "static/images/python_for_beginners_sololearn.png",
+    "Typing Speed":                     "static/images/typingSpeed.png",
+    "HTML Sololearn":                   "static/images/html_sololearn.jpg",
+    "CSS Hackerrank":                   "static/images/css_hackerrank.png",
+    "JavaScript Sololearn":             "static/images/js_sololearn.png"
+};
+
+const my_skills_jsx = (<>
+    {Object.entries(my_skills).map(([key, value], index) => (
+        <div key={index} className="certificate_img"><img src={value} alt={key} /></div>
+))}
+</>)
+
+
 function Skill() {
 
     return (<>
@@ -10,28 +29,7 @@ function Skill() {
             <h1>Skills</h1>
             <p>Some of my certificates in programming and and web development </p>
             <div className="certificates_container">
-                <div className="certificate_img"><img src="static/images/hackerrank_python_basic.png" alt=""/></div>
-                <div className="certificate_img"><img src="static/images/python_core_sololearn.png" alt=""/></div>
-                <div className="certificate_img"><img src="static/images/python_intermediate_sololearn.png" alt=""/></div>
-                <div className="certificate_img"><img src="static/images/DS_python_sololearn.png" alt=""/></div>
-                <div className="certificate_img"><img src="static/images/python_for_beginners_sololearn.png" alt=""/></div>
-                <div className="certificate_img"><img src="static/images/typingSpeed.png" alt=""/></div>
-                <div className="certificate_img"><img src="static/images/html_sololearn.jpg" alt=""/></div>
-                <div className="certificate_img"><img src="static/images/css_hackerrank.png" alt=""/></div>
-                <div className="certificate_img"><img src="static/images/js_sololearn.png" alt=""/></div>
-            </div>
-            <div className="view_certificate" id="view_certificate" onclick="close_view_certificate()">
-                <div>
-                    <div className="certificate_header" id="certificate_header">
-                        <div className="chtitle" id="chtitle">title</div>
-                        <div className="chclose" id="chclose" onclick="close_view_certificate()"><i
-                            className="fa-solid fa-x"></i></div>
-                    </div>
-                    <div className="certificate_main">
-                        <img src="" alt="certificate" id="certificate_main_img"/>
-                    </div>
-                    <div className="certificate_footer">footer</div>
-                </div>
+                {my_skills_jsx}
             </div>
         </section>
     </>)

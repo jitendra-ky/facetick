923df7bda11670d32f187eca3e67f89f825323a2
zsquare12 <36557466+zsquare12@users.noreply.github.com>
2024-05-21 18:22:42 +0530

    shartodo code link change

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,7 +30,7 @@ const projects = {
         imgAlt: "boy studying",
         skill: "React.JS + Django.PY",
         description: "SharkToDo is a React-based to-do list application designed to help you effortlessly manage your tasks.",
-        codeLink: "https://www.smileplease.life/sharktodo.html"
+        codeLink: "https://zsquare12.github.io/new_tempelate/"
     },
     TicTakToe: {
         imgSrc: "static/icons/tictaktoe.png",

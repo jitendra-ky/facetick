87111dec992dd2108783a7314e97da5f787c0c83
zsquare12 <36557466+zsquare12@users.noreply.github.com>
2024-08-09 20:36:48 +0530

    side-bar toggle feature added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,19 @@
 import './Header.css';
+import { useState } from 'react';
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(true);
+
+    // here is the toggle function
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
+    const menuClassUp = menuOpen ? 'line one open' : 'line one';
+    const menuClassDown = menuOpen ? 'line two open' : 'line two';
+    const sidebarClass = menuOpen ? 'sidebar' : 'sidebar close';
+
+
     const output = (
         <header className="container">
             <div className="left">
@@ -13,9 +26,15 @@ function Header() {
                 <div className="three">three</div>
             </div>
             <div className="right">
-                <div className="one icon"></div>
-                <div className="two icon"></div>
-                <div className="three icon"></div>
+                {/* <div className="one icon"></div>
+                <div className="two icon"></div> */}
+                <div className="three nav-menu" onClick={toggleMenu}>
+                    <div className={menuClassUp}></div>
+                    <div className={menuClassDown}></div>
+                </div>
+                <div className={sidebarClass}>
+                    <div className="container"></div>
+                </div>
             </div>
         </header>
     );

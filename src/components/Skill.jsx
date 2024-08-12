d9e0e58e6472a1e0c3bdbc59466715b0f1d7b996
zsquare12 <36557466+zsquare12@users.noreply.github.com>
2024-08-12 16:14:35 +0530

    style section design done

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,16 +3,32 @@ import './Skill.css'
 function Skill() {
 
     const skillSets = [
-        { skill: 'Frontend Development', level: 'Expert' },
-        { skill: 'Backend Development', level: 'Intermediate' },
-        { skill: 'Database Management', level: 'Advanced' },
+        { skill: 'Frontend Development', skill_list: ['HTML', 'CSS', 'JavaScript', 'React'] },
+        { skill: 'Backend Development', skill_list: ['Python', 'Django', 'Django REST Framework'] },
+        { skill: 'Database Management', skill_list: ['MySQL', 'SQLite', 'RDMS'] },
         // Add more skill sets as needed
     ];
 
     const skillItems = skillSets.map((skillSet, index) => (
-        <div key={index} className="skill-item glass">
-            <div className="skill-name">{skillSet.skill}</div>
-            <div className="skill-level">{skillSet.level}</div>
+        <div className="skill-item-parent glass">
+            <div key={index} className="skill-item">
+                <div className="skill-card-title">{skillSet.skill}</div>
+                <div className="skill-card-body">
+                    <ul>
+                        {skillSet.skill_list.map((skill, index2) => (
+                            <li key={index2} className='tag'>{skill}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            <div className="divider"></div>
+            <div className="skill-detail">
+                <ul>
+                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, omnis!</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius.</li>
+                </ul>
+            </div>
         </div>
     ));
 
